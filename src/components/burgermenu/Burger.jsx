afc538167e6a9e786d22fcef1a7a6ae521061537
Kris Amerman <amerman.k@northeastern.edu>
2022-03-03 02:50:53 -0500

    Burger menu

---
+++ src/components/burgermenu/Burger.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import Menu from '../menu/Menu';
+import './burger.scss';
+
+
+const Burger = () => {
+    const [status, setStatus] = useState('open');
+    return (
+        <div 
+            className='Burger__container'
+            role='button'
+            onClick= {() => setStatus(status === 'open' ? 'close' : 'open')}
+        >
+            <i className={status}></i>
+            <i className={status}></i>
+            <i className={status}></i>
+
+            <nav className={status}>
+                <div id="Burger__menu">
+                    <Menu />
+                </div>
+            </nav>
+
+        </div>
+        
+    );
+};
+
+export default Burger;
---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Menu = () => (
+    <>
+    <p><a href="#home">Home</a></p>
+    <p><a href="#wgpt3">Portfolio</a></p>
+    <p><a href="#possibility">Projects</a></p>
+    <p><a href="#features">Research</a></p>
+    <p><a href="#blog">About</a></p>
+    </>
+)
+
+export default Menu
